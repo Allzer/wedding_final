535cffe4a89057748bac1658b26dc15a8e32744b
Allzer <109965453+Allzer@users.noreply.github.com>
2024-04-24 17:24:56 +0300

    Первые приготовления

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+from flask_migrate import Migrate
+
+from config import SECRET_KEY, DATABASE_URL
+from database import db
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html', title='Главная страница')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/Guests/model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import db
+
+class Guests(db.Model):
+
+    __tablename__ = 'guest'
+
+    id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(20), nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    patronymic = db.Column(db.String(20), nullable=False)
+    contact_id = db.Column(db.Integer, ForeignKey('contact.contact_id'), unique=True)
+
+    contact = relationship("Contacts", uselist=False, back_populates="guest")
+
+class Contacts(db.Model):
+
+    __tablename__ = 'contact'
+
+    contact_id = db.Column(db.Integer, primary_key=True)
+    telegram = db.Column(db.String(70), nullable=True)
+    vk = db.Column(db.String(70), nullable=True)
+    p_number = db.Column(db.Integer, nullable=False)
+
+    guest = relationship("Guests", back_populates="contact")
