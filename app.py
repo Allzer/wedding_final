41d684d4425e694476fe076eefc507fa1d828ba8
Allzer <109965453+Allzer@users.noreply.github.com>
2024-06-15 23:44:44 +0300

    Добавил версию для компа

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import SECRET_KEY
 
 
@@ -6,9 +6,13 @@ app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
 @app.route("/index")
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html', title='Главная страница')
+    user_agent = request.user_agent.string
+    if 'Mobile' in user_agent or 'Android' in user_agent or 'iPhone' in user_agent:
+        return render_template('index.html')
+    else:
+        return render_template('index_desktop.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
